d2423554379c4b8796a8e299ccd4b9c8e155e9d2
Sam Nie <147653722+SamNie2027@users.noreply.github.com>
2024-07-09 17:07:59 -0400

    Added php files to offer methods for retrieval of individual values (get[value-name]) from the database (preliminary stumps)

---
+++ src/retrieve/retrieve-viewprofile.php
@@ -0,0 +1,25 @@
+<?php
+
+function getFirstName($email) {
+
+}
+
+function getLastName($email) {
+
+}
+
+function getYear($email) {
+
+}
+
+function getContacts($email) {
+
+}
+
+function getLookingFor($email) {
+
+}
+
+function getBio($email) {
+
+}
