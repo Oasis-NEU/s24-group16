6dec1517befc84fb4e921b5bcecbd582bcf22890
Sam Nie <147653722+SamNie2027@users.noreply.github.com>
2024-08-06 15:09:21 -0400

    Complete functionality to showing classmates and viewing classmate profiles!

--- src/ViewProfile.php
+++ src/ViewProfile.php
@@ -1,4 +1,6 @@
-<!-- This is the view profile page which you can get to by clicking "Profile" on the navbar-->
+<!-- This is the view profile page which you can get to by clicking "Profile" on the navbar
+ 
+Or you can get to by clicking someone's name on the YourClasses.php page.-->
 
 <?php
 
@@ -10,10 +12,22 @@ require __DIR__ . '/retrieve/retrieve-viewprofile.php';
 //The navbar is 'embedded' here and includes all the standard script/ style links as well.
 include_once __DIR__ . '/navbar.php';
 
+$editPerms = false;
+
+//Gets the correct email from the url message
+if (isset($_GET['profile'])) {
+    if ($_GET['profile'] == 'own') {
+        $email = $_SESSION['email'];
+        $editPerms = true;
+    } else {
+        $email = base64_decode($_GET['profile']);
+    }
+}
+
 ?>
 
 <!--Display for the profile-->
-<div class="text-light" style="height: 85vh; background-color: #86DBFF; position: relative;">
+<div class="text-light" style="height: 85vh; background-color: #86DBFF; ">
     <div class="d-flex justify-content-center align-items-center"
         style="height: 86vh; width: 100vw; position: absolute;">
 
@@ -21,26 +35,30 @@ include_once __DIR__ . '/navbar.php';
         <div class="bg-white view-profile-box font-primary text-center" style="width: 20vw; padding-top: 5vh;">
             <h1 style="font-size: 6vh;">
                 <?php
-                getFirstName();
+                getFirstName($email);
                 echo " ";
-                getLastName();
+                getLastName($email);
                 ?>
             </h1>
             <p class="view-profile-font-sizing" style="margin-top:5vh;">
-                Year <?php getYear();
+                Year <?php getYear($email);
                 echo " ";
-                getMajor(); ?> student
+                getMajor($email); ?> student
             </p>
             <p class="view-profile-font-sizing" style="margin-top:10vh;">Contacts:
             <div class="view-profile-font-sizing">
-                <?php getContacts(); ?>
+                <?php getContacts($email); ?>
             </div>
             </p>
             <div style="height: 8vh;">
             </div>
-            <a href="EditProfile.php">
-                <button class="btn btn-theme-orange" style="border-radius: 50px; font-size: 3vh;">Edit Profile</button>
-            </a>
+            <?php
+                if ($editPerms) {
+                    echo "<a href='EditProfile.php'>";
+                    echo "<button class='btn btn-theme-orange' style='border-radius: 50px; font-size: 3vh;'>Edit Profile</button>";
+                    echo "</a>";
+                }
+            ?>
         </div>
 
         <!-- The right display section (looking for, bio) -->
@@ -48,12 +66,12 @@ include_once __DIR__ . '/navbar.php';
             margin-left: 5vw; padding-top: 7vh; padding-left: 5vw;">
             <p class="view-profile-font-sizing">Looking for a buddy that:</p>
             <div class="view-profile-font-sizing" style="height: 20vh;">
-                <?php getLookingFor(); ?>
+                <?php getLookingFor($email); ?>
             </div>
             <p class="view-profile-font-sizing">
                 Bio:
             <div class="view-profile-font-sizing">
-                <?php getBio(); ?>
+                <?php getBio($email); ?>
             </div>
             </p>
             <div style="height: 20vh;">
